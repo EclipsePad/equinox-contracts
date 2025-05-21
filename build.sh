f3b09a85f81e3dffc4a2df7fe081cf6a41d57f57
M. Daeva <embedmad@gmail.com>
2025-05-21 20:05:19 +0300

    enabled unstake

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cargo cw-optimizoor
 # docker run --rm -v "$(pwd)":/code \
 #   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/target \
 #   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-#   cosmwasm/rust-optimizer:0.15.1
+#   cosmwasm/optimizer:0.16.1
 
 # rename wasm files
 cd artifacts
